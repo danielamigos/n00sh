0561b7e2570365ddd851149f885f2343b3d82bce
Daniel Valenzuela <daniel.valenzuela@4thsource.com>
2016-04-06 18:06:11 -0500

    Commit please

--- wp-content/themes/noosh/template-frontpage.php
+++ wp-content/themes/noosh/template-frontpage.php
@@ -1,59 +1,69 @@
 <?php /* Template Name: Frontpage Template */ get_header(); ?>
 
-    <div class="row" style="padding-bottom:12px;">
-        <div class="col-md-6" style="padding-right:12px;">
-            <img alt="image1" src="<?php echo get_template_directory_uri(); ?>/img/noosh-landing-img-2.jpg" />
+    <div class="row" >
+        <div class="col-md-6" style="padding-right:12px; padding-bottom:12px;">
+            <img alt="image1" src="<?php echo get_template_directory_uri(); ?>/img/noosh-landing-img-2.jpg" style="width:100%"/>
         </div>
         <div class="col-md-6" style="padding-left:12px;">
             <div class="row"> 
                 <div class="col-md-6 h-md-6 h-xs-12" >
                     <div  style="padding-right:12px; padding-bottom:12px;">
-                        <div style="background-color:brown; height:100%;"></div>
+                        <div style="background-color:#68411c; height:100%;"></div>
                     </div>
                 </div>
                 <div class="col-md-6" style="padding-left:12px;">
-                    <img alt="image3" src="<?php echo get_template_directory_uri(); ?>/img/noosh-landing-img-1.jpg" />
+                    <img alt="image3" src="<?php echo get_template_directory_uri(); ?>/img/noosh-landing-img-1.jpg"  style="width:100%"/>
                 </div>
             </div>
             <div class="row" style="padding-top:12px;"> 
                 <div class="col-md-6" style="padding-right:12px;">
-                    <img alt="image4" src="<?php echo get_template_directory_uri(); ?>/img/noosh-landing-img-3.jpg" />
+                    <img alt="image4" src="<?php echo get_template_directory_uri(); ?>/img/noosh-landing-img-3.jpg"  style="width:100%"/>
                 </div>
-                <div class="col-md-6" style="padding-left:12px;">
-                    <img alt="image5" src="<?php echo get_template_directory_uri(); ?>/img/yellow-box.png" />
+                <div class="col-md-6 h-md-6 h-xs-12" >
+                    <div  style="padding-left:12px; padding-bottom:12px;">
+                        <div style="background-color:#ffd24f; height:100%;"></div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="row" style="padding-top:12px; padding-bottom:12px;">
-        <div class="col-md-3" style="padding-right:12px;">
-            <img alt="image6" src="<?php echo get_template_directory_uri(); ?>/img/beige-box.png" />
+    <div class="row" style="padding-top:12px;">
+        <div class="col-md-3 h-md-3 col-xs-3 h-xs-3" >
+            <div style="padding-right:12px; padding-bottom:12px;">
+                <div style="background-color:#e8d3a2; height:100%;"></div>
+            </div>
         </div>
-        <div class="col-md-9" style="padding-left:12px;">
-            <img alt="image7" src="<?php echo get_template_directory_uri(); ?>/img/noosh-landing-img-4.jpg" />
+        <div class="col-md-9" style="padding-left:12px; padding-bottom:12px;">
+            <img alt="image7" src="<?php echo get_template_directory_uri(); ?>/img/noosh-landing-img-4.jpg"  style="width:100%"/>
         </div>
     </div>
-    <div class="row" style="padding-top:12px; padding-bottom:12px;">
+    <div class="row" style="padding-top:12px; ">
         <div class="col-md-6" style="padding-right:12px;">
-            <img alt="image8" src="<?php echo get_template_directory_uri(); ?>/img/noosh-landing-img-5.jpg" />
+            <img alt="image8" src="<?php echo get_template_directory_uri(); ?>/img/noosh-landing-img-5.jpg"  style="width:100%"/>
         </div>
-        <div class="col-md-6" style="padding-left:12px;">
-            <img alt="image9" src="<?php echo get_template_directory_uri(); ?>/img/red-rectangle.png" />
+        <div class="col-md-6 h-md-3 col-xs-12 h-xs-12" >
+            <div  style="padding-left:12px; padding-bottom:12px;">
+                <div style="background-color:#ed3124; height:100%;"></div>
+            </div>
         </div>
     </div>
     <div class="row" style="padding-top:12px; ">
         <div class="col-md-9" style="padding-right:12px;">
-            <img alt="image10" src="<?php echo get_template_directory_uri(); ?>/img/noosh-landing-img-6.jpg" />
+            <img alt="image10" src="<?php echo get_template_directory_uri(); ?>/img/noosh-landing-img-6.jpg"  style="width:100%"/>
         </div>
-        <div class="col-md-3" style="padding-left:12px;">
+        <div class="col-md-3">
             <div class="row">
-                <div class="col-md-12" style="padding-bottom:12px;">
-                    <img alt="image11" src="<?php echo get_template_directory_uri(); ?>/img/light-brown-box.png" />
+                <div class="col-md-12 h-md-12 col-xs-12 h-xs-12" >
+                    <div  style="padding-left:12px; padding-bottom:12px;">
+                        <div style="background-color:#b1651d; height:100%;"></div>
+                    </div>
                 </div>
             </div>
             <div class="row" style="padding-top:12px;">
-                <div class="col-md-12">
-                    <img alt="image12" src="<?php echo get_template_directory_uri(); ?>/img/yellow-box.png" />
+                <div class="col-md-12 h-md-12 col-xs-12 h-xs-12" >
+                    <div  style="padding-left:12px; padding-bottom:12px;">
+                        <div style="background-color:#ffd24f; height:100%;"></div>
+                    </div>
                 </div>
             </div>
         </div>
