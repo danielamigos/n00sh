39b6d2d8e2190e07e6b1e43ac08f879f0ffc53c1
Daniel Valenzuela <danielamigos@gmail.com>
2016-04-07 00:09:47 -0500

    commit please

--- wp-content/themes/noosh/template-frontpage.php
+++ wp-content/themes/noosh/template-frontpage.php
@@ -1,68 +1,93 @@
 <?php /* Template Name: Frontpage Template */ get_header(); ?>
 
-    <div class="row" >
-        <div class="col-md-6" style="padding-right:12px; padding-bottom:12px;">
+    <div class="row first-level-row" >
+        <div class="col-xs-12 col-sm-6 first-level-solid-column-left" <?PHP //style="padding-right:12px; padding-bottom:12px;" ?> >
             <img alt="image1" src="<?php echo get_template_directory_uri(); ?>/img/noosh-landing-img-2.jpg" style="width:100%"/>
         </div>
-        <div class="col-md-6" style="padding-left:12px;">
-            <div class="row"> 
-                <div class="col-md-6 h-md-6 h-xs-12" >
-                    <div  style="padding-right:12px; padding-bottom:12px;">
-                        <div style="background-color:#68411c; height:100%;"></div>
+        <div class="col-xs-12 col-sm-6 first-level-hollow-column-right" <?PHP //style="padding-left:12px;"?> >
+            <div class="row second-level-row"> 
+                <div class="col-xs-6 h-xs-6" >
+                    <div class="second-level-column-left"  <?PHP //style="padding-right:12px; padding-bottom:12px;"?> >
+                        <div style="background-color:#68411c; height:100%;">
+                            <p class="light-fancy-text">Introducing the first Squeezable Almond Butter</p>
+                        </div>
                     </div>
                 </div>
-                <div class="col-md-6" style="padding-left:12px;">
+                <div class="col-xs-6 second-level-column-right" <?PHP //style="padding-left:12px;"?> >
                     <img alt="image3" src="<?php echo get_template_directory_uri(); ?>/img/noosh-landing-img-1.jpg"  style="width:100%"/>
                 </div>
             </div>
-            <div class="row" style="padding-top:12px;"> 
-                <div class="col-md-6" style="padding-right:12px;">
+            <div class="row second-second-level-row" <?PHP //style="padding-top:12px;"?> > 
+                <div class="col-xs-6 second-level-column-left" <?PHP //style="padding-right:12px;?> ">
                     <img alt="image4" src="<?php echo get_template_directory_uri(); ?>/img/noosh-landing-img-3.jpg"  style="width:100%"/>
                 </div>
-                <div class="col-md-6 h-md-6 h-xs-12" >
-                    <div  style="padding-left:12px; padding-bottom:12px;">
-                        <div style="background-color:#ffd24f; height:100%;"></div>
+                <div class="col-xs-6 h-xs-6" >
+                    <div class="second-level-column-right"  <?PHP //style="padding-left:12px; padding-bottom:12px;"?> >
+                        <div style="background-color:#ffd24f; height:100%;">                            
+                            <p class="dark-fancy-text">It's Ok.<br/>Indulge.<br/>We love it too.</p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="row" style="padding-top:12px;">
-        <div class="col-md-3 h-md-3 col-xs-3 h-xs-3" >
-            <div style="padding-right:12px; padding-bottom:12px;">
-                <div style="background-color:#e8d3a2; height:100%;"></div>
+    
+    
+    <div class="row first-level-row" <?php //style="padding-top:12px;"?> >
+        <div class="col-xs-12 h-xs-6 col-sm-3  h-sm-3 " >
+            <div class="first-level-solid-column-left" <?php //sstyle="padding-right:12px; padding-bottom:12px;"?> >
+                <div style="background-color:#e8d3a2; height:100%;">                     
+                    <p class="dark-fancy-title" style="padding-top:20%; padding-bottom:0;">Craving Noosh?</p>
+                    <hr style="border-width:3px; margin-left:30px;margin-right:30px;"/>
+                    <p style="text-align:center; color: #ed3124; font-family: 'AvantGardeBold-Regular'; font-size: 14px;padding-left:30px;padding-right:30px;">Satisfy your almond butter cravings. Noosh squeezable almond butter is available at your local Costco.<br/><br/>
+                    <button style="width: 109px; height: 31px; border-radius: 8px; background-color: #ed3124;border-style: hidden; color:#ffffff;font-family: 'AvantGardeBold-Regular';font-size: 16px; text-transform: uppercase;">FIND</button></p>
+                </div>
             </div>
         </div>
-        <div class="col-md-9" style="padding-left:12px; padding-bottom:12px;">
-            <img alt="image7" src="<?php echo get_template_directory_uri(); ?>/img/noosh-landing-img-4.jpg"  style="width:100%"/>
+        <div class="col-xs-12 col-sm-9 first-level-solid-column-right"  <?php //sstyle="padding-left:12px; padding-bottom:12px;"?> >
+            <img alt="image7" src="<?php echo get_template_directory_uri(); ?>/img/noosh-landing-img-4-2.jpg"  style="width:100%"/>
         </div>
     </div>
-    <div class="row" style="padding-top:12px; ">
-        <div class="col-md-6" style="padding-right:12px;">
-            <img alt="image8" src="<?php echo get_template_directory_uri(); ?>/img/noosh-landing-img-5.jpg"  style="width:100%"/>
+    
+    
+    <div class="row first-level-row"  <?php //style="padding-top:12px; "?> >
+        <div class="col-xs-12 col-sm-6 first-level-solid-column-left"  <?php //style="padding-right:12px;"?> >
+            <img alt="image8" src="<?php echo get_template_directory_uri(); ?>/img/noosh-landing-img-5-2.jpg"  style="width:100%"/>
         </div>
-        <div class="col-md-6 h-md-3 col-xs-12 h-xs-12" >
-            <div  style="padding-left:12px; padding-bottom:12px;">
-                <div style="background-color:#ed3124; height:100%;"></div>
+        <div class="col-xs-12 h-xs-6 col-sm-6 h-sm-3" >
+            <div class="first-level-solid-column-right"  <?php //style="padding-left:12px; padding-bottom:12px;"?> >
+                <div style="background-color:#ed3124; height:100%;">
+                    <p class="white-fancy-title" style="padding-top:10%; padding-bottom:0;">Almond Crunch Ice Cream</p>
+                    <hr style="border-width:3px; margin-left:30px;margin-right:30px;"/>
+                    <p style="text-align:center; color: #ffffff; font-family: 'AvantGardeBold-Regular'; font-size: 14px;padding-left:30px;padding-right:30px;">All-natural almond flavored ice cream. Loaded with chocolate almond 
+clusters and drizzled with Noosh squeezable almond butter<br/><br/>
+                    <button style="width: 109px; height: 31px; border-radius: 8px; background-color: #ffd24f;border-style: hidden; color:#68411c;font-family: 'AvantGardeBold-Regular';font-size: 16px; text-transform: uppercase;">RECIPE</button></p>
+                </div>
             </div>
         </div>
     </div>
-    <div class="row" style="padding-top:12px; ">
-        <div class="col-md-9" style="padding-right:12px;">
-            <img alt="image10" src="<?php echo get_template_directory_uri(); ?>/img/noosh-landing-img-6.jpg"  style="width:100%"/>
+    
+        
+    <div class="row first-level-row"  <?php //style="padding-top:12px; "?> >
+        <div class="col-xs-12 col-sm-9 first-level-solid-column-left"  <?php //style="padding-right:12px;"?> >
+            <img alt="image10" src="<?php echo get_template_directory_uri(); ?>/img/noosh-landing-img-6-2.jpg"  style="width:100%"/>
         </div>
-        <div class="col-md-3">
-            <div class="row">
-                <div class="col-md-12 h-md-12 col-xs-12 h-xs-12" >
-                    <div  style="padding-left:12px; padding-bottom:12px;">
-                        <div style="background-color:#b1651d; height:100%;"></div>
+        <div class="col-xs-12 col-sm-3 first-level-hollow-column-right">
+            <div class="row second-level-row">
+                <div class="col-xs-12 h-xs-6 col-sm-12 h-sm-12" >
+                    <div class="second-level-column-solo"  <?php //style="padding-left:12px; padding-bottom:12px;"?> >
+                        <div style="background-color:#b1651d; height:100%;">                            
+                            <p class="light-fancy-text-2">Squeeze it.<br/>Eat it.<br/>Share it.</p>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="row" style="padding-top:12px;">
-                <div class="col-md-12 h-md-12 col-xs-12 h-xs-12" >
-                    <div  style="padding-left:12px; padding-bottom:12px;">
-                        <div style="background-color:#ffd24f; height:100%;"></div>
+            <div class="row second-second-level-solo-row"  <?php //style="padding-top:12px;"?> >
+                <div class="col-xs-12 h-xs-6 col-sm-12 h-sm-12" >
+                    <div class="second-level-column-solo"  <?php //style="padding-left:12px; padding-bottom:12px;"?> >
+                        <div style="background-color:#ffd24f; height:100%;">                                
+                            <p class="dark-fancy-text">Enjoy a better breakfast with Noosh.</p>
+                        </div>
                     </div>
                 </div>
             </div>
