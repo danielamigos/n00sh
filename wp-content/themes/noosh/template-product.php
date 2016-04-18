a22839eb067e00b1c6c1a4878b0e4567e9f7586b
Daniel Valenzuela <danielamigos@gmail.com>
2016-04-17 22:35:12 -0500

    barely started product page

---
+++ wp-content/themes/noosh/template-product.php
@@ -0,0 +1,31 @@
+<?php /* Template Name: Product Template */ get_header(); ?>
+        
+    <div class="row first-level-row"  <?php //style="padding-top:12px; "?> >
+        <div class="col-xs-12 col-sm-3 first-level-hollow-column-left">
+            <div class="row second-level-row">
+                <div class="col-xs-12 h-xs-6 col-sm-12 h-sm-12" >
+                    <div class="second-level-column-solo"  <?php //style="padding-left:12px; padding-bottom:12px;"?> >
+                        <div style="background-color:#68411c;" class="h-content v-wrap">                            
+                            <p class="light-fancy-text-2 v-box" >Meet the folks<br/>behind Noosh<br/>Squeezable Almond<br/>Butter.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row second-second-level-solo-row"  <?php //style="padding-top:12px;"?> >
+                <div class="col-xs-12 h-xs-6 col-sm-12 h-sm-12" >
+                    <div class="second-level-column-solo"  <?php //style="padding-left:12px; padding-bottom:12px;"?> >
+                        <div style="background-color:#ed3124;" class="h-content v-wrap">                                
+                            <p class="white-text v-box" >Our family has farmed almonds for over 90 years. Since 2011 our state-of-the-art manufacturing facility located in Simi Valley California, has been producing silky smooth almond butter. Needless to say, we know a lot about almonds and we’re passionate about what we do.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-9 first-level-solid-column-right"  <?php //style="padding-right:12px;"?> >
+            <img alt="image10" src="<?php echo get_template_directory_uri(); ?>/img/noosh-landing-img-6-2.jpg"  style="width:100%"/>
+        </div>
+    </div>
+	
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
